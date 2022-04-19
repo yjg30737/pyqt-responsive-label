ca946aec1f564408d0b14c399fcf082cf87b3ae6
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-19 13:13:34 +0900

    Set font size more accurate (even if the way to do it depends on brute force)

--- pyqt_responsive_label/responsiveLabel.py
+++ pyqt_responsive_label/responsiveLabel.py
@@ -1,6 +1,4 @@
-import math
-
-from PyQt5.QtWidgets import QLabel, QSizePolicy
+from PyQt5.QtWidgets import QLabel
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
@@ -11,18 +9,28 @@ class ResponsiveLabel(QLabel):
         self.__parent = parent
         if self.__parent:
             self.__parent.installEventFilter(self)
+
         self.installEventFilter(self)
         self.__initUi()
 
     def __initUi(self):
         self.setAlignment(Qt.AlignCenter)
-        self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
 
     def __setFontSizeAccordanceWithWindow(self):
         dpr = self.devicePixelRatio()
         self.setFont(QFont('Arial', min(200 // dpr, max(10, self.widthMM() // dpr))))
-        self.setFont(QFont('Arial', min(200 // dpr, max(10, math.floor(
-            self.widthMM() // dpr * self.width() / self.fontMetrics().boundingRect(self.text()).width())))))
+        # todo generalize, not brute force (if possible)
+        # self.setFont(QFont('Arial', min(200 // dpr, max(10, math.floor(self.widthMM() // dpr * self.width() / self.fontMetrics().boundingRect(self.text()).width())))))
+
+        while self.width() / self.fontMetrics().boundingRect(self.text()).width() <= 1.0:
+            font = self.font()
+            font.setPointSize(font.pointSize()-1)
+            self.setFont(font)
+
+        while self.height() / self.fontMetrics().boundingRect(self.text()).height() <= 0.7:
+            font = self.font()
+            font.setPointSize(font.pointSize()-1)
+            self.setFont(font)
 
     def eventFilter(self, obj, e):
         if isinstance(obj, type(self.__parent)):
