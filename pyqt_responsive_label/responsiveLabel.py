872d5c5ce499b5a8a0d6d0fb01fe4d73746d4b09
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-19 13:24:27 +0900

    Refactoring

--- pyqt_responsive_label/responsiveLabel.py
+++ pyqt_responsive_label/responsiveLabel.py
@@ -16,31 +16,34 @@ class ResponsiveLabel(QLabel):
     def __initUi(self):
         self.setAlignment(Qt.AlignCenter)
 
-    def __setAccurateFont(self):
+    def __setApproximateFontSize(self):
+        dpr = self.devicePixelRatio()
+        self.setFont(QFont('Arial', min(200 // dpr, max(10, self.widthMM() // dpr))))
+
+    def __setAccurateFontSize(self):
         font = self.font()
         font.setPointSize(font.pointSize() - 1)
         self.setFont(font)
 
     def __setAccurateFontWidth(self):
         while self.width() / self.fontMetrics().boundingRect(self.text()).width() <= 1.0:
-            self.__setAccurateFont()
+            self.__setAccurateFontSize()
 
     def __setAccurateFontHeight(self):
         while self.height() / self.fontMetrics().boundingRect(self.text()).height() <= 0.7:
-            self.__setAccurateFont()
+            self.__setAccurateFontSize()
 
     def __setFontSizeAccordanceWithWindow(self):
-        dpr = self.devicePixelRatio()
-
         # Set the label font size accordance with windows size "approximately"
-        self.setFont(QFont('Arial', min(200 // dpr, max(10, self.widthMM() // dpr))))
+        self.__setApproximateFontSize()
+
+        # Set the label font size accordance with windows size "accurately"
 
         # todo generalize, not brute force (if possible)
         # self.setFont(QFont('Arial', min(200 // dpr, max(10, math.floor(self.widthMM() // dpr * self.width() / self.fontMetrics().boundingRect(self.text()).width())))))
 
         # Set the label font size smaller than affordable width
         self.__setAccurateFontWidth()
-
         # Set the label font size smaller than affordable height
         self.__setAccurateFontHeight()
 
