d58ed1e37277f6add09f93269bd52b4db834598f
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-09 21:35:58 +0900

    Update setup.py for add pypi description

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
+import codecs
+import os
+
 from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
 
 setup(
     name='pyqt-responsive-label',
@@ -9,6 +17,8 @@ setup(
     packages=find_packages(),
     description='PyQt QLabel which can resize the font responsively accordance with window\'s size change',
     url='https://github.com/yjg30737/pyqt-responsive-label.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
     install_requires=[
         'PyQt5>=5.8'
     ]
