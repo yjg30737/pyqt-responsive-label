5a04bd6eff73df3ec41775dd10f0e4f4835b1204
Jung Gyu Yoon <yjg30737@gmail.com>
2022-06-29 08:46:28 +0900

    Remove some functions to enhance performance, fix bug (size getting much bigger than needed)

--- pyqt_responsive_label/responsiveLabel.py
+++ pyqt_responsive_label/responsiveLabel.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QLabel, QSizePolicy
+from PyQt5.QtWidgets import QLabel, QSizePolicy, qApp
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 
@@ -20,36 +20,15 @@ class ResponsiveLabel(QLabel):
     def __initUi(self):
         self.setAlignment(Qt.AlignCenter | Qt.AlignVCenter)
         self.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        self.setMinimumSize(self.sizeHint())
 
     def __setApproximateFontSize(self):
-        dpr = self.devicePixelRatio()
-        self.setFont(QFont('Arial', min(200 // dpr, max(10, self.widthMM() // dpr))))
-
-    def __setAccurateFontWidth(self):
-        while self.width() / self.fontMetrics().boundingRect(self.text()).width() <= 1.0:
-            font = self.font()
-            font.setPointSize(font.pointSize() - 1)
-            self.setFont(font)
-
-    def __setAccurateFontHeight(self):
-        font_size = min(self.height(), self.fontMetrics().boundingRect(self.text()).height()) // 2
-        font = self.font()
-        font.setPointSize(font_size)
-        self.setFont(font)
+        dpr = qApp.screens()[0].logicalDotsPerInch() / 96.0
+        self.setFont(QFont('Arial', max(12, min(self.widthMM()-2, self.height()-2) // dpr)))
 
     def __setFontSizeAccordanceWithWindow(self):
         # Set the label font size accordance with windows size "approximately"
         self.__setApproximateFontSize()
-
-        # Set the label font size accordance with windows size "accurately"
-
-        # todo generalize, not brute force (if possible)
-        # self.setFont(QFont('Arial', min(200 // dpr, max(10, math.floor(self.widthMM() // dpr * self.width() / self.fontMetrics().boundingRect(self.text()).width())))))
-
-        # Set the label font size smaller than affordable width
-        self.__setAccurateFontWidth()
-        # Set the label font size smaller than affordable height
-        self.__setAccurateFontHeight()
 
     def setAcceptTextChange(self, f: bool):
         self.__text_change_flag = f
